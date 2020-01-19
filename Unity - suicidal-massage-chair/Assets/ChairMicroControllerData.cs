51948d2451e7d6c420c766a22574b06254a4f01b
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-01-19 16:51:55 +0100

    Setting up basic arduino chair and controller classes

---
+++ Unity - suicidal-massage-chair/Assets/ChairMicroControllerData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ChairMicroControllerState
+{
+    public float chair_estimated_position;
+    public bool chair_up_on;
+    public bool chair_down_on;
+    [Header("Roller")]
+    public bool roller_kneading_on;
+    public float roller_kneading_speed;
+    public bool roller_pounding_on;
+    public float roller_pounding_speed;
+    public bool roller_up_on;
+    public bool roller_down_on;
+    public bool roller_top_sensor;
+    public bool roller_bottom_sensor;
+    [Range(0, 1)]
+    public float roller_position;
+    [Header("Feet Roller")]
+    public bool feet_roller;
+    public bool feet_roller_on;
+    [Range(0, 1)]
+    public float feet_roller_speed;
+    [Header("Airpump")]
+    public bool airpump_on;
+    public bool airbag_shoulders_on;
+    public bool airbag_arms_on;
+    public bool airbag_legs_on;
+    public bool airbag_outside_on;
+    [Header("Other")]
+    public bool butt_vibration_on;
+    public bool backlight_on;
+    public Color backlight_color;
+    public bool redgreen_statuslight_red;
+    public bool redgreen_statuslight_green;
+}
---
+++ Unity - suicidal-massage-chair/Assets/Script/ChairOutputVisualizer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairOutputVisualizer : MonoBehaviour
+{
+    public ChairMicroControllerState data;
+    public Sprite chair;
+    [Header("Chair Vertical Position")]
+    [Range(0, 1)]
+    public float chair_estimated_position;
+    public Sprite chair_up_on;
+    public Sprite chair_down_on;
+    [Header("Roller")]
+    public Sprite roller_kneading_on;
+    public float roller_kneading_speed;
+    public Sprite roller_pounding_on;
+    public float roller_pounding_speed;
+    public Sprite roller_up_on;
+    public Sprite roller_down_on;
+    public Sprite roller;
+    public Sprite roller_top_sensor;
+    public Sprite roller_bottom_sensor;
+    [Range(0, 1)]
+    public float roller_position;
+    [Header("Feet Roller")]
+    public Sprite feet_roller;
+    public Sprite feet_roller_on;
+    [Range(0, 1)]
+    public float feet_roller_speed;
+    [Header("Airpump")]
+    public Sprite airpump_on;
+    public Sprite airbag_shoulders_on;
+    public Sprite airbag_arms_on;
+    public Sprite airbag_legs_on;
+    public Sprite airbag_outside_on;
+    [Header("Other")]
+    public Sprite butt_vibration_on;
+    public Sprite backlight_on;
+    public Color backlight_color;
+    public Sprite redgreen_statuslight_red;
+    public Sprite redgreen_statuslight_green;
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
