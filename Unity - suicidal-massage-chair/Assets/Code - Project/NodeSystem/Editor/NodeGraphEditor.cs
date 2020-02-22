33ec73bc98b677819ba28021e809afae862f3468
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-02-22 15:48:52 +0100

    Added aggressive repaint and node filter

---
+++ Unity - suicidal-massage-chair/Assets/Code - Project/NodeSystem/Editor/NodeGraphEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
+using XNodeEditor;
+
+[XNodeEditor.NodeGraphEditor.CustomNodeGraphEditorAttribute(typeof(NodeGraph))]
+public class NodeGraphEditor : XNodeEditor.NodeGraphEditor
+{
+    public override string GetNodeMenuName(Type type)
+    {
+        if (type.IsSubclassOf(typeof(BaseNode)))
+            return base.GetNodeMenuName(type);
+        return null;
+    }
+
+    public override void OnGUI()
+    {
+        // Keep repainting the GUI of the active NodeEditorWindow
+        NodeEditorWindow.current.Repaint();
+    }
+}
