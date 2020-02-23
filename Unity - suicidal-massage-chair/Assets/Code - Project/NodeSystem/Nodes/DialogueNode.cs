258040dd632d5702ffc8485097ec195ff6f0f7ed
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-02-23 12:12:15 +0100

    Fixed a bug while playing nodes

--- Unity - suicidal-massage-chair/Assets/Code - Project/NodeSystem/Nodes/DialogueNode.cs
+++ Unity - suicidal-massage-chair/Assets/Code - Project/NodeSystem/Nodes/DialogueNode.cs
@@ -25,7 +25,7 @@ public class DialogueNode : BaseNode
         Data.OnFinished += OnFinished;
 
         NodeFunctionRunner.Instance.StopAllCoroutines();
-        NodeFunctionRunner.Instance.StartCoroutine(Data.InvokeFunctions());
+        NodeFunctionRunner.Instance.StartCoroutine(Data.InvokeFunctionsAndPlayAudio(name));
     }
 
     private void OnInterrupted(UserInputUp e)
@@ -72,7 +72,6 @@ public class DialogueNode : BaseNode
         var node = (BaseNode)port.Connection.node;
         NodeGraph.PlayNode(node);
     }
-
 
     public override void OnNodeDisable()
     {
