c491fd65c04292a817d5f295fc434381a641d0e3
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-02-24 07:32:13 +0100

    Added serial controller ui

---
+++ Unity - suicidal-massage-chair/Assets/SerialControllerUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialControllerUI : MonoBehaviour
+{
+    public SerialController Controller;
+
+    public void ChangeComPort(string com)
+    {
+        Controller.portName = com;
+    }
+}
