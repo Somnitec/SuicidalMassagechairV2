6d2d934d145df3a708b5d9778080b344c5832309
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-03-06 14:45:02 +0100

    Added node text display

---
+++ Unity - suicidal-massage-chair/Assets/NodeTextUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NodeTextUI : MonoBehaviour
+{
+    
+    public TextMeshProUGUI Text;
+
+    public NodeGraph Graph => SettingsHolder.Instance.Settings.Graph;
+    
+    void Update()
+    {
+        var text = "";
+
+        DialogueNode dialogueNode = Graph.Current as DialogueNode;
+        if (dialogueNode != null)
+            text += $"{dialogueNode.Data.Data.Text}";
+
+        if (Text != null)
+            Text.text = text;
+    }
+}
