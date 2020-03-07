9d697292609b56d41b3a37ef79ccc43adb6a927d
Arvid <arvid@arvidandmarie.com>
2020-03-07 13:36:13 +0100

    forgot to save

--- Arduino - suicidal-massage-chair/Controller/ControllerSuicidalMassageChair2.0.ino/receiveMessage.ino
+++ Arduino - suicidal-massage-chair/Controller/ControllerSuicidalMassageChair2.0.ino/receiveMessage.ino
@@ -1,3 +1,41 @@
+/*
+
+{
+  'controllerCommand':'...'
+  'controllerValue':"...' 0..1-5
+}
+
+
+InputParse
+  buttonKill, => bool
+  buttonCustomA, => bool
+  buttonCustomB, => bool
+  buttonCustomC, => bool
+  buttonSettings,  => bool
+  buttonThumbUp,
+  buttonThumbDown,
+  buttonYes,
+  buttonNo,
+  buttonRepeat,
+  buttonHorn,
+  languageSet
+  slider => int 1..5 
+  
+InfoParse  
+  customScreenA => string
+  customScreenB => string
+  customScreenC => string
+  clearScreen = bool
+  buttonBounceTime = int in MS
+  buttonFadeTimeSettings = int in MS
+  buttonBrightnessSettings = 0 .. 255
+  allLeds = bool
+  settingsLed = bool
+  yesLed  = bool
+  noLed = bool
+  reset
+*/
+
 bool readingMessage = false;
 String readString;
 int maxStringLength = 64;
