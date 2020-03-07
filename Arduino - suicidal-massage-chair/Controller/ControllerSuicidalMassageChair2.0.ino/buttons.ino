9001fc7cf2a02810d9e26c40b06309d763820f78
Arvid <arvid@arvidandmarie.com>
2020-03-07 13:59:27 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Arduino - suicidal-massage-chair/Controller/ControllerSuicidalMassageChair2.0.ino/buttons.ino
@@ -0,0 +1,37 @@
+void readButtons() {
+  for (int i = 0; i < buttonAmount; i++)
+  {
+    // Update the Bounce instance :
+    debouncedButtons[i].update();
+    if (i == 0)
+    { //kill is NC
+      if (debouncedButtons[i].rose())
+      {
+        //Serial.print(buttonsString[i]);
+        //Serial.println();
+        sendCommand( buttonsString[i], "BOOMBA");
+      }
+    }
+    else if (i == 4)
+    { //language is switch
+      if (debouncedButtons[i].rose() || debouncedButtons[i].fell())
+      {
+        //Serial.print(buttonsString[i]);
+        //Serial.print(" = ");
+        //Serial.print(debouncedButtons[i].read());
+        //Serial.println();
+
+sendCommand( buttonsString[i],debouncedButtons[i].read());
+
+      }
+    }
+    else if (debouncedButtons[i].fell())
+    {
+      //Serial.print(buttonsString[i]);
+      //Serial.println();
+      sendCommand( buttonsString[i], "triggered");
+      //cmdMessenger.sendCmd(kSendInput, "triggered");
+    }
+  }
+
+}
---
+++ Arduino - suicidal-massage-chair/Controller/ControllerSuicidalMassageChair2.0.ino/receiveMessage.ino
@@ -0,0 +1,66 @@
+bool readingMessage = false;
+String readString;
+int maxStringLength = 64;
+String last_command = "";
+
+void readSerial() {
+  if (Serial.available())  {
+    char c = Serial.read();  //gets one byte from serial buffer
+    if (c == '{' && !readingMessage)readingMessage = true;
+    if (readingMessage) {
+      if (c == '}') {  //looks for end of data packet marker
+        readString += c;
+        receiveMessage(readString); //prints string to serial port out
+        //do stuff with captured readString
+        readString = ""; //clears variable for new input
+        readingMessage = false;
+      }
+      else {
+        readString += c; //makes the string readString
+        if (readString.length() > maxStringLength) {//preventing buffer overflow
+          readString = "";
+          readingMessage = false;
+          printError("overflow");
+        }
+      }
+
+    }
+  }
+}
+
+void printError(String error) {
+  Serial.print(F("{\n\t\"error\":\""));
+  Serial.print(error);
+  Serial.println(F("\"\n}"));
+}
+
+
+bool validateInput(String command, int expectedArguments) {
+  if ( expectedArguments == 0) return false;//always get one argument (for now)
+  else {
+    for (int i = 0; i < expectedArguments; i++) {
+      String item = doc[command][i];
+      if (item.equals("null"))return false;
+    }
+  }
+  last_command = command;
+  return true;
+}
+
+void receiveMessage( String message) {
+  last_command = "";
+
+  DeserializationError error = deserializeJson(doc, message);
+  // Test if parsing succeeds.
+  if (error) {
+    printError(error.c_str());
+    return;
+  }
+
+
+
+
+  if (validateInput(F("test"), 1)) {
+    sendCommand("test","received");
+  }
+}
