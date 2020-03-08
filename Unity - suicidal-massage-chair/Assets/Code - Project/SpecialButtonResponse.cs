8423b8079fb4dd57a543b2a6615980fbf409fc73
Alexander van der Zalm <alexandervdzalm@gmail.com>
2020-03-08 14:32:43 +0100

    Added volume slider functionality

--- Unity - suicidal-massage-chair/Assets/Code - Project/SpecialButtonResponse.cs
+++ Unity - suicidal-massage-chair/Assets/Code - Project/SpecialButtonResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Framework;
 using Input;
@@ -9,10 +10,28 @@ public class SpecialButtonResponse : MonoBehaviour
 {
     private Settings settings => SettingsHolder.Instance.Settings;
     private NodeGraph graph => settings.Graph;
+    private AudioManager audioManager => AudioManager.Instance;
 
     private void Start()
     {
         Events.Instance.AddListener<SpecialInput>(HandleSpecialInput);
+        SetUpVolume();
+    }
+
+    [Button]
+    private void SetUpVolume()
+    {
+        if (settings.VolumeLevels == null || settings.VolumeLevels.Count < 5)
+        {
+            settings.VolumeLevels = new List<float>()
+            {
+                0.2f,
+                0.4f,
+                0.6f,
+                0.8f,
+                1.0f,
+            };
+        }
     }
 
     private void HandleSpecialInput(SpecialInput special)
@@ -34,7 +53,31 @@ public class SpecialButtonResponse : MonoBehaviour
             case SpecialInputButtons.Settings:
                 SettingsNode();
                 break;
+            case SpecialInputButtons.One:
+                Volume(0);
+                break;
+            case SpecialInputButtons.Two:
+                Volume(1);
+                break;
+            case SpecialInputButtons.Three:
+                Volume(2);
+                break;
+            case SpecialInputButtons.Four:
+                Volume(3);
+                break;
+            case SpecialInputButtons.Five:
+                Volume(4);
+                break;
+            case SpecialInputButtons.Horn:
+                break;
+            default:
+                break;
         }
+    }
+
+    private void Volume(int settingIndex)
+    {
+        audioManager.SetVolume(settings.VolumeLevels[settingIndex]);
     }
 
     private void SettingsNode()
